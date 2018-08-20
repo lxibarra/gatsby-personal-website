bc012213179742ddc58d61e7de2969f69383ed73
Ricardo Ibarra <coder10@gmail.com>
2018-08-19 20:06:37 -0700

    Added responsive classes (#3)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import Link from 'gatsby-link';
 import Header from '../components/header/header';
 import Footer from '../components/footer/Footer';
@@ -21,22 +22,42 @@ const IndexPage = () => (
             </BlockContent>
           </FlexContent>
       </Flex>
-      <Flex>
-          <FlexContent flexGrow="1" textAlign="justify">
-            <BlockContent
-              title="Education"
-              paragraph="Got my college degree and masters on Information Technology with a specialty on database design.
-                        Mostly worked with data warehouse designs and Business intelligence tools.">
-            </BlockContent>
-          </FlexContent>
-          <FlexContent flexGrow="1" textAlign="justify" margin="0 0 0 35px">
-            <BlockContent
-              title="Technologies"
-              paragraph="On my early years i worked with the LAMP stack, then moved to C# & Sql Server mostly for webapps.
-              The last couple of years i have worked with Backbone, Node, Angular and React.">
-            </BlockContent>
-          </FlexContent>
-      </Flex>
+      <MediaQuery query="(min-width: 900px)">
+        <Flex>
+            <FlexContent flexGrow="1" textAlign="justify">
+              <BlockContent
+                title="Education"
+                paragraph="Got my college degree and masters on Information Technology with a specialty on database design.
+                          Mostly worked with data warehouse designs and Business intelligence tools.">
+              </BlockContent>
+            </FlexContent>
+            <FlexContent flexGrow="1" textAlign="justify" margin="0 0 0 35px">
+              <BlockContent
+                title="Technologies"
+                paragraph="On my early years i worked with the LAMP stack, then moved to C# & Sql Server mostly for webapps.
+                The last couple of years i have worked with Backbone, Node, Angular and React.">
+              </BlockContent>
+            </FlexContent>
+        </Flex>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 899px)">
+        <Flex direction="column">
+            <FlexContent flexGrow="1" textAlign="justify">
+              <BlockContent
+                title="Education"
+                paragraph="Got my college degree and masters on Information Technology with a specialty on database design.
+                          Mostly worked with data warehouse designs and Business intelligence tools.">
+              </BlockContent>
+            </FlexContent>
+            <FlexContent flexGrow="1" textAlign="justify">
+              <BlockContent
+                title="Technologies"
+                paragraph="On my early years i worked with the LAMP stack, then moved to C# & Sql Server mostly for webapps.
+                The last couple of years i have worked with Backbone, Node, Angular and React.">
+              </BlockContent>
+            </FlexContent>
+        </Flex>
+      </MediaQuery>
       <Flex>
           <FlexContent flexGrow="1">
             <BlockContent
