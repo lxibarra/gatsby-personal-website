482cfdf4f0a83d7ccb3e0955e02dbb02855a8fba
Ricardo Ibarra <riibarra@stubhub.com>
2018-08-19 18:02:12 -0700

    Added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,121 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "dir": "ltr",
+        "lang": "English",
+        "name": "@ricardoibarrai Fullstack web developer",
+        "scope": "/",
+        "display": "browser",
+        "start_url": "https://doitfy.com/",
+        "short_name": "doitfy",
+        "theme_color": "transparent",
+        "description": "Hi im Ricardo frontend engineer using Angular & React to develop scalable web applications",
+        "orientation": "any",
+        "background_color": "transparent",
+        "related_applications": [],
+        "prefer_related_applications": false,
+        "icons": [
+            {
+                "src": "src/images/2ce7940e-f277-db4c-3c79-0a17ee570401.webPlatform.png",
+                "sizes": "44x44",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/930ccf1b-42e8-d3be-b1da-f39b173feea7.webPlatform.png",
+                "sizes": "48x48",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/3f5e8bbf-be49-2e56-1507-400d2e74863d.webPlatform.png",
+                "sizes": "1240x600",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/4315fab7-ceaf-90d0-eff7-844d7abe260e.webPlatform.png",
+                "sizes": "300x300",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/29c8620d-db09-61fd-7de2-8063a0011d64.webPlatform.png",
+                "sizes": "150x150",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/d4e54f16-af94-7754-6458-24aa67870dd0.webPlatform.png",
+                "sizes": "88x88",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/078c6909-5ef0-0399-1eea-22969bfd0a4a.webPlatform.png",
+                "sizes": "24x24",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/b9e7c885-1fb0-116c-7f1c-017a36e6c933.webPlatform.png",
+                "sizes": "50x50",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/e10dd8d4-f603-7c50-5c9e-fa24b829dddd.webPlatform.png",
+                "sizes": "620x300",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/4c3ef3c4-860b-c4f5-257c-e9ed8a2e1518.webPlatform.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/8e8e9851-58e6-bb3b-e9cf-55952e26308b.webPlatform.png",
+                "sizes": "144x144",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/7449a916-82da-7128-624c-d413eedb0c37.webPlatform.png",
+                "sizes": "96x96",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/f08bdbc8-dc6d-b4f7-7638-d2f7a978c4a4.webPlatform.png",
+                "sizes": "72x72",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/1a475843-e08e-f634-bfcb-7f076983144c.webPlatform.png",
+                "sizes": "36x36",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/1c151eff-0191-5894-9058-2d0ff2b68217.webPlatform.png",
+                "sizes": "1024x1024",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/728b90bc-e6d7-28e7-ee6b-b2b6ec091ab3.webPlatform.png",
+                "sizes": "180x180",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/eeda6607-a6f9-7b12-3092-d2c30ec06fa7.webPlatform.png",
+                "sizes": "152x152",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/df114919-c020-3515-8702-bd62aaa05f42.webPlatform.png",
+                "sizes": "120x120",
+                "type": "image/png"
+            },
+            {
+                "src": "src/images/e1de1391-c8c9-d092-bd90-5c944d6153e7.webPlatform.png",
+                "sizes": "76x76",
+                "type": "image/png"
+            }
+        ]
+      },
+  },
     'gatsby-plugin-offline'
   ],
 };
